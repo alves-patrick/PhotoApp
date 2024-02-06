7086f2a6a6d86e252f15fb2f7ce40c95952da667
Patrick Alves <patrickalvesarts@gmail.com>
2024-02-06 20:33:31 -0300

    User create

--- PhotoApp/ContentView.swift
+++ PhotoApp/ContentView.swift
@@ -7,18 +7,84 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+func screenSize() -> CGSize {
+    return UIScreen.main.bounds.size
 }
 
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                NavigationBarView()
+                VStack {
+                    HStack {
+                        Image(uiImage: UIImage(imageLiteralResourceName: "profile"))
+                            .resizable()
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .frame(width: 36, height: 36)
+                        VStack(alignment: .leading) {
+                            Text("Mille_F")
+                                .font(.footnote)
+                                .fontWeight(.bold)
+                            HStack {
+                                Text("London, England")
+                                    .font(.footnote)
+                                    .foregroundColor(.secondary)
+                                Spacer()
+                                Text("2 minutes ago")
+                                    .font(.footnote)
+                                    .foregroundColor(.primary)
+                            }
+                            
+                            
+                        }
+                        
+                    }
+            
+                    Image(uiImage: UIImage(imageLiteralResourceName: "lake"))
+                        .resizable()
+                        .frame(height: 300)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .shadow(color: .black, radius: 15, x: 5, y: 10)
+                }
+                .padding(.leading)
+                .padding(.trailing)
+                Spacer()
+            }
+        }
+    }
+}
 #Preview {
     ContentView()
+}
+
+struct NavigationBarView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "camera")
+                        .resizable()
+                        .frame(width: 25, height: 20)
+                }
+                .foregroundColor(.black)
+                Spacer()
+                Text("PhotoApp")
+                    .font(Font.custom("Billabong", size: 28))
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(uiImage: UIImage(imageLiteralResourceName: "profile"))
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 36, height: 36)
+                        .clipShape(Circle())
+                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                    
+                    
+                }
+            }
+            .padding()
+        }
+    }
 }
