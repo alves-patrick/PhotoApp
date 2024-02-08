2ee0fdbab4077a120ab545f2d2c70dfe70ee071b
Patrick Alves <patrickalvesarts@gmail.com>
2024-02-08 13:28:20 -0300

    Fix Cardview, Navigationbar

---
+++ PhotoApp/Views/NavigationBarView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBarView.swift
+//  PhotoApp
+//
+//  Created by Patrick Alves on 2/8/24.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBarView()
+}
