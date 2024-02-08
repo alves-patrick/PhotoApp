9ce92be9e63d2f1e9dedda04119ba64871fd99c3
Patrick Alves <patrickalvesarts@gmail.com>
2024-02-08 06:05:05 -0300

    Finnaly cardview

---
+++ PhotoApp/Views/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  PhotoApp
+//
+//  Created by Patrick Alves on 2/8/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
